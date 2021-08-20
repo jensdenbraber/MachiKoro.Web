7bf0fae268a78829bfb654a00a2dcbc36fe245d3
Jens den Braber <jens.denbraber@gmail.com>
2021-08-20 22:55:18 +0200

    basic outline of objects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+import { Button, ButtonGroup, Grid, Paper, Typography, Slider, Input } from '@material-ui/core';
+import Card from './components/card'
+import Deck from './components/deck'
+import Player from './components/player'
+
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+//   },
+// }));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Grid container>
+      <Grid item>
+
+          <Button>
+            Klikken!!!
+          </Button>
+          <Player></Player>
+      </Grid>
+      <Grid item>
+        <Deck></Deck>
+        <Deck></Deck>
+        <Deck></Deck>
+      </Grid>
+      <Grid item>
+
+        <Button>
+          Klikken!!!
+        </Button>
+        <Player></Player>
+      </Grid>
+            
+     </Grid>
+
     </div>
   );
 }
---
+++ src/components/playerInventory.js
@@ -0,0 +1,4 @@
+export default function PlayerInventory() {
+
+    return <PlayerInventory></PlayerInventory> 
+}
